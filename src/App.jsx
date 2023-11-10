c9db7933144223bab7f4f57c959cfbde0312c2a1
rougixxx <m.remmane@esi-sba.dz>
2023-11-10 12:03:18 +0100

    creating routers and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIN'
+import SignUp from './pages/SignUp'
+import ForgetPassword from './pages/ForgotPassword'
+import Offers from './pages/Offers'
+
 
 function App() {
 
@@ -9,9 +16,17 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1 className='text-2xl text-red-500'>hello mate</h1>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/sign-in" element={<SignIn/>}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/forget-password" element={<ForgetPassword/>}/>
+          <Route path="/offers" element={<Offers/>}/>
+
+        </Routes>
+      </Router>
     
     </>
   )
