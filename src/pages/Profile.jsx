258ed7a69562a67461f766e6f87a56ff6d13624b
rougixxx <m.remmane@esi-sba.dz>
2023-11-13 16:06:52 +0100

    change details is completed in prodile page + the middleware for the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react"
-import {getAuth} from "firebase/auth"
+import {getAuth, updateProfile} from "firebase/auth"
 import { useNavigate } from "react-router"
+import { toast } from "react-toastify"
+import { doc, updateDoc } from "firebase/firestore"
+import { db } from "../firebase"
 
 export default function Profile() {
     const auth = getAuth()
     const navigate = useNavigate()
+    const [changeDetails, setChangingDetails] = useState(false)
+
     
 
     const [formData, setFormData] = useState({
@@ -17,6 +22,31 @@ export default function Profile() {
         auth.signOut()
         navigate("/")
     }
+    function onChange(e) {
+        setFormData((prev) =>  ({
+            ...prev,
+            [e.target.id]: e.target.value
+        }))
+           
+        
+    }
+    async function onSubmit() {
+        try {
+            if (auth.currentUser.displayName !== name) {
+                await updateProfile(auth.currentUser, {
+                    displayName: name,
+                })
+                const docRef = doc(db, "users", auth.currentUser.uid )
+                await updateDoc(docRef, {
+                    name
+                })
+            }
+            toast.success("Profile details updated")
+
+        } catch(error) {
+            toast.error("we could not apply the changes for your Profile")
+        }
+    }
     return (
         <>
         <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -26,8 +56,9 @@ export default function Profile() {
                 <form  >
 
                     {/* Name input */}
-                    <input type="text" id="name" value={name} disabled 
-                    className="w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"/>
+                    <input type="text" id="name" value={name} disabled={!changeDetails} onChange={onChange}
+                    className={`w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out ${changeDetails && "bg-red-200 focus:bg-red-200"}`}/>
+
                     {/* email input */}
                     <input type="email" id="email" value={email} disabled
                         className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transistion ease-in-out"
@@ -35,7 +66,12 @@ export default function Profile() {
                      <div className="flex justify-between whitespace-nowrap text-sm sm:text-xl mb-6">
                         <p className="flex items-center">
                             Do you want to change your name?
-                            <span className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">Edit</span>
+                            <span className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+                            onClick={ () => {
+                                changeDetails && onSubmit();
+                                setChangingDetails((prev) => !prev)}} >
+                                {changeDetails ? "Apply Changes": "Edit"}
+                              </span>
                         </p>
                         <p className="text-blue-600 hover:text-blue-700 transition duration-200 ease-in-out cursor-pointer"
                         onClick={onLogout}>Sign Out</p>
